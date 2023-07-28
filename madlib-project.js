28977766adb11122004449514bdd7e3d2c62e96a
pgu15 <140416871+pgu15@users.noreply.github.com>
2023-07-28 10:26:31 -0400

    Add files via upload

---
+++ madlib-project.js
@@ -0,0 +1,11 @@
+let adj1 = "huge ";
+let noun1 = "dog";
+let name1 = "Daniel's";
+let noun2 = "pizza";
+let num1 = 4;
+let noun3 = "slices";
+let noun4 = "home";
+let fin = "relaxed";
+
+let story = "Last night, I went to my friend’s house and was surprised by his " + adj1 + noun1 + ".  Later that evening, we went to " + name1 + " house and we had some amazing " + noun2 + ".  I had a total of " + num1 + " " +  noun3 + ".   After that, I went back " + noun4 +  "and just " + fin + ". ";
+console.log(story);
